08ac27e6a6e742ca93731ded4220b8642ce704b4
Chris D <chris@deweese.com>
2022-06-16 21:05:40 -0500

    Counting up with large numbers and saving item count

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,8 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance = null;
+
+    public LargeNumber itemCount = new LargeNumber();
 
     private void Awake()
     {
@@ -14,5 +17,23 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject);
         
         DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        itemCount = itemCount.StringToLargeNumber(PlayerPrefs.GetString("ItemCount", "0"));
+        
+        InvokeRepeating(nameof(SaveGame), 60, 60);
+    }
+
+    private void SaveGame()
+    {
+        //TODO: USE STRINGS.CS
+        PlayerPrefs.SetString("ItemCount", itemCount.LargeNumberToString());
+    }
+
+    private void OnApplicationQuit()
+    {
+        SaveGame();
     }
 }
--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,9 +1,24 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class UIController : MonoBehaviour
 {
-    
+    public TextMeshProUGUI itemCountLabel;
+
+    private void Start()
+    {
+        itemCountLabel.text = PlayerPrefs.GetString("ItemCount", "0");
+    }
+
+    public void onClick_AddItem()
+    {
+        GameManager.instance.itemCount.AddOne();
+
+        itemCountLabel.text = GameManager.instance.itemCount.LargeNumberToShortString();
+
+        //TODO: SUBSCRIBE CLICK TO EVENT
+    }
 }
