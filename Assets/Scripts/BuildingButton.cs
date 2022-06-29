b2467c296f81a9b5b44cf149a3884aa9a22977ee
Chris D <chris@deweese.com>
2022-06-28 23:28:30 -0500

    Create BuildingButton.cs

---
+++ Assets/Scripts/BuildingButton.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuildingButton : MonoBehaviour
+{
+    public Building building;
+    public Text nameLabel;
+    public Text priceLabel;
+    public Text ownedLabel;
+    public Button button;
+}
